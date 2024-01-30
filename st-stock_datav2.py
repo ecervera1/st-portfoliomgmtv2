0f424a841194018cbba148a27d108601fd2944ab
ecervera1 <157933641+ecervera1@users.noreply.github.com>
2024-01-30 13:33:23 -0600

    Update st-stock_datav2.py

--- st-stock_datav2.py
+++ st-stock_datav2.py
@@ -394,5 +394,6 @@ if st.sidebar.checkbox("Cash Flow"):
 
 if st.sidebar.checkbox("Calculate FCFF and FCFE"):
     fcff_fcfe_results = calculate_fcff_and_fcfe(selected_stock)
-    st.write(fcff_fcfe_results)
+    #st.write(fcff_fcfe_results)
+    st.dataframe(fcff_fcfe_results)
 
