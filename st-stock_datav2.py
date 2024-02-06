460cb97c3aaadfa68a786c91d33bf5292aaa85cd
ecervera1 <157933641+ecervera1@users.noreply.github.com>
2024-02-06 16:06:09 -0600

    Update st-stock_datav2.py

--- st-stock_datav2.py
+++ st-stock_datav2.py
@@ -648,6 +648,7 @@ if st.sidebar.checkbox('Portflio', value=False):
     df[['Middle Part', 'Dollar Amount', 'Percentage']] = df['Current Value % of Account'].apply(split_current_value).apply(pd.Series)
     new_column_names = {'Middle Part': 'Current Value', 'Dollar Amount': 'Cost', 'Percentage': 'Percentage of Portfolio'}
     df.rename(columns=new_column_names, inplace=True)
+    df=df.iloc[:, [1, 3,4,5,6,7]]
     
     # Filter UI
     industries = df['Industry'].unique()
