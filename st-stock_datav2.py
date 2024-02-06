efffd69b9a0d336ea59583fd236f59725b61bf64
ecervera1 <157933641+ecervera1@users.noreply.github.com>
2024-02-06 15:26:34 -0600

    Update st-stock_datav2.py

--- st-stock_datav2.py
+++ st-stock_datav2.py
@@ -618,8 +618,9 @@ if st.sidebar.checkbox('Portflio', value=False):
         #df = pd.read_csv('Portfolio Positions_02022024_10.csv')
         df = pd.read_csv('Portfolio Positions_02022024_10.csv', usecols=lambda col: col != 'Unnamed: 0')
     
-    # Fetch the industry for each symbol and add it as a column
-    df['Industry'] = df['Symbol'].apply(get_industry)
+        # Fetch the industry for each symbol and add it as a column
+        df['Industry'] = df['Symbol'].apply(get_industry)
+        return df
     
     # Streamlit script starts here
     st.title('Portfolio Analysis by Industry')
