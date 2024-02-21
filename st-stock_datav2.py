025706de4f9acb0a2496b6d51b0c1f6b539322fb
ecervera1 <157933641+ecervera1@users.noreply.github.com>
2024-02-20 18:13:22 -0600

    Update st-stock_datav2.py

--- st-stock_datav2.py
+++ st-stock_datav2.py
@@ -624,7 +624,7 @@ if st.sidebar.checkbox('Portflio', value=False):
         # Function to load the data and add industry information
         def load_data():
             # Load your data here
-            df = pd.read_csv('Portfolio Positions_02092024.csv')
+            df = pd.read_csv('SMIF Position_02162024.csv')
             #df = pd.read_csv('Portfolio Positions_02092024.xlsx', usecols=lambda col: col != 'Unnamed: 0')
         
             # Fetch the industry for each symbol and add it as a column
